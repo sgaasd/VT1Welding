4d3659124ca5438f888d1367d008af717c136832
Simon Sunesen Gaasdal <sgaasd19@student.aau.dk>
2022-11-17 09:49:35 +0100

    Update ur5socket.py

--- ur5socket.py
+++ ur5socket.py
@@ -1,5 +1,21 @@
     
 import socket
+import os
+from datetime import datetime
+import numpy as np
+
+
+def savedata(testtype,data,rating):
+    print("saving data")
+    today= datetime.today()
+    testname=str(today.year)+str(today.month)+str(today.day)
+    curdir = os.getcwd()
+    number=len(os.listdir(curdir+"\Data\weld"))+1
+    number=f"{number:03d}"
+    testname=str(testname)+"_"+str(rating)+"_"+str(testtype)+"_"+str(number)+".csv"
+    print(testname)
+    np.savetxt("Data/"+str(testtype)+"/"+testname,data,delimiter=",")
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 50000))
@@ -9,20 +25,34 @@ conn, addr = s.accept()
 print(conn)
 print(addr)
 
-data = conn.recv(8)
-print(data)
-acc = 0.1
-speed = 0.2
-time_m = 0
-blend = 0
+
+close = False
+
+while close == False:
+    if start: #Start til før svejsning
+        conn.send((bytes('(1)', 'ascii'))) #Sender signal til roboten om den skal starte
+        data = conn.recv(16) # forventer at modtaget et signal om at den er kørt til svejseposition
+        if data == "(readyToWeld)":
+            readyToWeld = True
+            start = False
+            datalist = ["Current, Voltage, Wire-feed"]
+            
+    if startSvejsning and readyToWeld: #Start svejsning og begynder data indsamling
+        conn.send((bytes('(2)', 'ascii')))
+        data = conn.recv(16)
+        datalist.append(data)
+        if conn.recv(16) == "weldment_Done":
+            weldment = "Done"
+
+    if weldment == "Done": #Gemmer data
+        savedata(testtype="weld", data=datalist, rating=1)
+        weldment =="NotDone"
+
+    if closeSocket == True:
+        close == True
 
 
-    
-#conn.send((bytes('(9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999)', 'ascii')))
-while True:
-    conn.send((bytes('(1)', 'ascii')))
-    data = conn.recv(16)
-    
-    print(data)
+conn.close()
 
-#conn.close()
+
+
