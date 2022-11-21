53cd9fa7bd9b7a02455613b385d3b20cac132f8c
Christopher Endelt <77574340+Krille945@users.noreply.github.com>
2022-11-21 12:00:17 +0100

    Update Microphones.py

--- Microphones.py
+++ Microphones.py
@@ -1,26 +1,53 @@
 from multiprocessing.connection import wait
 import sounddevice as sd
 from scipy.io.wavfile import write
+import time
+import numpy as np
+
+list=sd.query_devices()
+print(list)
+sd.default.device=14,4
 def CallMic(seconds,fs):
    # fs = 44100  # Sample rate
     # seconds = 3  # Duration of recording
 
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-    sd.wait()  # Wait until recording is finished
-    
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=6)
+
     write('output.wav', fs, myrecording)  # Save as WAV file 
+    return myrecording
 
-CallMic(3,44100)
+#array=CallMic(10,16000)
 
-def startmic(fs):
-    myrecording = sd.rec(samplerate=fs, channels=2)
-
-    wait(5)
-
+def stoprec(array):
     sd.stop()
-    write('output2.wav', fs, myrecording) 
+    print(array)
 
-startmic(44100)
+    np.savetxt("raw.csv", array, delimiter=",")
+
+    print(len(array[0,:]))
+    datastop=0
+    for i in range(0,len(array[0,:])-1):
+        arrayflipped=np.flip(array[:,i],0)
+        datastoptemp=np.nonzero(arrayflipped)
+        datastoptemp=np.array(datastoptemp[0])
+        if datastop==0:
+            datastop=datastoptemp[0]
+        elif datastoptemp[0]<datastop:
+            datastop=datastoptemp[0]
+
+
+    print (datastop)
+
+    cutarray=array[0:len(array[:,0])-datastop,:]
+
+
+    np.savetxt("cut.csv", cutarray, delimiter=",")
+
+
+
+
+
+
 
 
 
