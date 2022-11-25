aad23cfdf18a646d7d07576a60cfd7979bea19a9
Christopher Endelt <77574340+Krille945@users.noreply.github.com>
2022-11-25 08:03:59 +0100

    Update cam.py

--- cam.py
+++ cam.py
@@ -2,30 +2,39 @@ import numpy as np
 import cv2 as cv
 import Microphones
 
-cap = cv.VideoCapture(1)
+#line 6-13 should be called before the loop starts
+cap = cv.VideoCapture(1) ###should be 1 with an external webcam
 # Define the codec and create VideoWriter object
 #fourcc = cv.VideoWriter_fourcc(*'XVID')
 #out = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
+fourcc = cv.VideoWriter_fourcc(*"mp4v")
+#fourcc = cv.VideoWriter_fourcc(*"h264")
+out = cv.VideoWriter('output.mp4', fourcc, 20.0, (640,  480)) #('name', encoder, framerate, (resolution)) This should be adjusted to fit the camera
+array=Microphones.CallMic(20,16000) #If microphones doesn't work check 9 in microphones to specify which device it uses. To get an overveiw of the devices look at the printed list
 
-#fourcc = cv.VideoWriter_fourcc(*"mp4v")
-fourcc = cv.VideoWriter_fourcc(*"h264")
-out = cv.VideoWriter('output.mp4', fourcc, 20.0, (640,  480))
-
-array=Microphones.CallMic(20,16000)
+##this is just for the counter 
 close= False
 close_counter=0
+
+
 while close== False:
+    ##this can be called at the start of the loop it will start showing the cam stream but wont save, which means you can adjust the camera
     ret, frame = cap.read()
     cv.waitKey(1)
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    frame = cv.flip(frame, 0)
-    # write the flipped frame
-    out.write(frame)
     cv.imshow('frame', frame)
+    if not ret:
+        print("Can't receive frame")
+        break
+
+
+    # write the flipped frame
+    ###### this should be called when we want to save data
+    out.write(frame)
+    
     if  close_counter>1000:
         break
+
+
     close_counter=close_counter+1
 # Release everything if job is finished
 Microphones.stoprec(array)
