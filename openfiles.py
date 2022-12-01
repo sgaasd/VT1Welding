4fb5b85175039053f435eb2e5b0768a980a7d66d
Christopher Endelt <77574340+Krille945@users.noreply.github.com>
2022-12-01 20:30:56 +0100

    added util func

--- openfiles.py
+++ openfiles.py
@@ -90,11 +90,40 @@ def path_of_vid():
     path="/"+data_dir+data_type[2]+cur_dir[len(cur_dir)-1]
     return path
 
+def uptime():
+    data_dir="Data/"
+    data_type=["cam/","info/","scan/","sound/","weld/"]
+    cur_dir=os.listdir(data_dir+data_type[4])
+    minlist=[]
+    maxlist=[]
+    timelist=[]
+    for i in cur_dir:
+        path=data_dir+data_type[4]+i
+        df = pd.read_csv(path, sep=',')
+        min=df['time [s]'].min()
+        max=df['time [s]'].max()
+        minlist.append(min)
+        maxlist.append(max)
+        for i in range(0,len(minlist)-1):
+            time=maxlist[i]-minlist[i]
+            timelist.append(time)
+    endtime=maxlist[-1]
+    startime=minlist[0]
+    totaltime=endtime-startime
+    util=(sum(timelist)/totaltime)*100
+    
+    print(timelist)
+    print(minlist)
+    print(str(round(util,2))+'%')
+    return path
+
+
 if __name__ == '__main__':
     df=updata_df(4)
     print(df)
     df_sound=updata_df_sound(3)
     print(df_sound)
-    open_scan()
+    #open_scan()
     vid=path_of_vid()
     print(vid)
+    uptime()
