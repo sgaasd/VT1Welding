1658fddd68aab25d60b1c4a90c9158c0b5b19325
Christopher Endelt <77574340+Krille945@users.noreply.github.com>
2022-12-05 21:09:08 +0100

    added description plus more

--- dash_data.py
+++ dash_data.py
@@ -26,25 +26,28 @@ colors = {
 past_len=0
 df=openfiles.updata_df(4)
 
-fig_vol=px.line(df,x="time [s]",y=' Voltage')
+fig_vol=px.line(df,x="time [s]",y=' Voltage',title='Voltage data')
 fig_vol.update_layout(
 plot_bgcolor=colors['background'],
 paper_bgcolor=colors['background'],
 font_color=colors['text'],
+margin=dict(l=20, r=20, t=35, b=20)
 )
 
-fig_cur=px.line(df,x="time [s]",y="Current")
+fig_cur=px.line(df,x="time [s]",y="Current",title='Current data')
 fig_cur.update_layout(
 plot_bgcolor=colors['background'],
 paper_bgcolor=colors['background'],
 font_color=colors['text'],
+margin=dict(l=20, r=20, t=35, b=20)
 )
 
-fig_wir=px.line(df,x="time [s]",y=" Wire-feed")
+fig_wir=px.line(df,x="time [s]",y=" Wire-feed",title='Wirer data')
 fig_wir.update_layout(
 plot_bgcolor=colors['background'],
 paper_bgcolor=colors['background'],
 font_color=colors['text'],
+margin=dict(l=20, r=20, t=35, b=20)
 )
 
 df=openfiles.updata_df_sound(3)
@@ -92,83 +95,111 @@ app.layout= html.Div(className="content", children=[
     dcc.Interval(id='update_interval', disabled=False, interval=1*1000, n_intervals=0),
     html.Div(className="left_menu",children=[
     html.H1('Data Visualisation of Robotic Welding',style={'margin-top':   '5px','margin-left':  '10px'}),
-    #html.Hr(style={'width': '95%'}),
+    html.Hr(style={'width': '95%'}),
     dcc.Graph(
     id='graph-vol',
-    figure=fig_vol,style={'width': '90%', 'height': '25%','text-align': 'center','padding':'1rem'}),
+    figure=fig_vol,style={'width': '90%', 'height': '20vh','text-align': 'center','padding':'1rem'}),
     html.Hr(style={'width': '95%'}),
     dcc.Graph(
     id='graph-cur',
-    figure=fig_cur,style={'width': '90%', 'height': '25%','text-align': 'center','padding':'1rem'}),
+    figure=fig_cur,style={'width': '90%', 'height': '20vh','text-align': 'center','padding':'1rem'}),
+    html.Hr(style={'width': '95%'}),
+    dcc.Graph(
+    id='graph-cur',
+    figure=fig_cur,style={'width': '90%', 'height': '20vh','text-align': 'center','padding':'1rem'}),
     html.Hr(style={'width': '95%'}),
     dcc.Graph(
     id='graph-wir',
-    figure=fig_wir,style={'width': '90%', 'height': '25%','text-align': 'center','padding':'1rem'})]),
+    figure=fig_wir,style={'width': '90%', 'height': '20vh','text-align': 'center','padding':'1rem'})]),
     html.Div(className="right_content",
     children=[
             html.Div(
             className="top_metrics",
             children=[html.Div(children=[
-            html.H1(children='Weld Information',style={
+            html.H1(children='Weld Information:',style={
                                'margin-top':   '0px',
                                'margin-left':  '0px',
+                               'margin-bottom':'0px',
                                'font-size': '25px'})]),
             html.Div([
-                dbc.Row([
-                    dbc.Col(html.Div(['Thickness bottom plate [mm]: ']), width=4),
-                    dbc.Col(html.Div(df_meta.iat[0,9]), width="auto")
+                dbc.Row([dbc.Col([
+                    dbc.Row([
+                        dbc.Col(html.Div(['Thickness bottom plate [mm]: ']), width=5),
+                        dbc.Col(html.Div(df_meta.iat[0,9]), width="auto")
+                    ]),
+                    dbc.Row([
+                        dbc.Col(html.Div(['Thickness vertical plate [mm]: ']), width=5),
+                        dbc.Col(html.Div(df_meta.iat[0,10]), width="auto")
+                    ]),
+                    dbc.Row([
+                        dbc.Col(html.Div(['Gas flow [L/min]: ']), width=5),
+                        dbc.Col(html.Div(df_meta.iat[0,14]), width="auto")
+                    ]),
+                    dbc.Row([
+                        dbc.Col(html.Div(['Wirer feed speed [m/min]: ']), width=5),
+                        dbc.Col(html.Div(df_meta.iat[0,13]), width="auto")
+                    ]),
+                    dbc.Row([
+                        dbc.Col(html.Div(['Input Current [A]: ']), width=5),
+                        dbc.Col(html.Div(df_meta.iat[0,11]), width="auto")
+                    ]),
+                    dbc.Row([
+                        dbc.Col(html.Div(['Input Voltage [V]']), width=5),
+                        dbc.Col(html.Div(df_meta.iat[0,12]), width="auto")
+                    ]),                
+                    dbc.Row([
+                        dbc.Col(html.Div(['Pass or fail: ']), width=5),
+                        dbc.Col(html.Div(df_meta.iat[0,5]), width="auto")
+                    ])
+            ]),             
+                dbc.Col([
+                    html.H1("Notes for test: ",style={
+                               'margin-top':   '0px',
+                               'margin-left':  '0px',
+                               'font-size': '20px'}),
+                    html.Div(df_meta.iat[0,15])
+                
                 ]),
-                dbc.Row([
-                    dbc.Col(html.Div(['Thickness vertical plate [mm]: ']), width=4),
-                    dbc.Col(html.Div(df_meta.iat[0,10]), width="auto")
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div(['Gas flow [L/min]: ']), width=4),
-                    dbc.Col(html.Div(df_meta.iat[0,14]), width="auto")
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div(['Wirer feed speed [m/min]: ']), width=4),
-                    dbc.Col(html.Div(df_meta.iat[0,13]), width="auto")
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div(['Input Current [A]: ']), width=4),
-                    dbc.Col(html.Div(df_meta.iat[0,11]), width="auto")
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div(['Input Voltage [V]']), width=4),
-                    dbc.Col(html.Div(df_meta.iat[0,12]), width="auto")
-                ]),                
-                dbc.Row([
-                    dbc.Col(html.Div(['Pass or fail: ']), width=4),
-                    dbc.Col(html.Div(df_meta.iat[0,5]), width="auto")
+                dbc.Col([
+                    html.H1("Description of test: ",style={
+                               'margin-top':   '0px',
+                               'margin-left':  '0px',
+                               'font-size': '20px'}),
+                    html.Div(df_meta.iat[0,16])
+                
                 ])
-            ])]
+                
+                ])
+
+            
+            ])
+            ]
 
         ),
         html.Div(className="bottem_right_menu",children=[html.H3(' Microphone data'),
             dcc.Graph(
                 id='graph-ch1',
-                figure=fig_ch1,style={'width': '100%', 'height': '20vh','text-align': 'center','padding':'0rem',}
+                figure=fig_ch1,style={'width': '100%', 'height': '19.9vh','text-align': 'center','padding':'0rem',}
             ),
             dcc.Graph(
                 id='graph-ch2',
-                figure=fig_ch2,style={'width': '100%', 'height': '20vh','text-align': 'center','padding':'0rem'}
+                figure=fig_ch2,style={'width': '100%', 'height': '19.9vh','text-align': 'center','padding':'0rem'}
             ),
             dcc.Graph(
                 id='graph-ch3',
-                figure=fig_ch3,style={'width': '100%', 'height': '20vh','text-align': 'center','padding':'0rem'}
+                figure=fig_ch3,style={'width': '100%', 'height': '19.9vh','text-align': 'center','padding':'0rem'}
             ),
             dcc.Graph(
                 id='graph-ch4',
-                figure=fig_ch4,style={'width': '100%', 'height': '20vh','text-align': 'center','padding':'0rem'}
+                figure=fig_ch4,style={'width': '100%', 'height': '19.9vh','text-align': 'center','padding':'0rem'}
             )
         ]
             
 
-        ),
-    ]),
+        )]),
 dcc.Store(id='determine_update')
 ])
+
 
 @app.callback(Output('graph-vol','figure'),Output('graph-cur','figure'),Output('graph-wir','figure'),Output('graph-ch1','figure'),Output('graph-ch2','figure'),Output('graph-ch3','figure'),Output('graph-ch4','figure'),Output('determine_update','data'),Input('update_interval','n_intervals'),Input('determine_update','data'))
 
@@ -180,25 +211,28 @@ def update_figs_live(n,past_len):
     if con==True:
         df=openfiles.updata_df(4)
 
-        fig_vol=px.line(df,x="time [s]",y=' Voltage')
+        fig_vol=px.line(df,x="time [s]",y=' Voltage',title='Voltage data')
         fig_vol.update_layout(
         plot_bgcolor=colors['background'],
         paper_bgcolor=colors['background'],
         font_color=colors['text'],
+        margin=dict(l=20, r=20, t=35, b=20)
         )
 
-        fig_cur=px.line(df,x="time [s]",y="Current")
+        fig_cur=px.line(df,x="time [s]",y="Current",title='Current data')
         fig_cur.update_layout(
         plot_bgcolor=colors['background'],
         paper_bgcolor=colors['background'],
         font_color=colors['text'],
+        margin=dict(l=20, r=20, t=35, b=20)
         )
 
-        fig_wir=px.line(df,x="time [s]",y=" Wire-feed")
+        fig_wir=px.line(df,x="time [s]",y=" Wire-feed",title='Wirer data')
         fig_wir.update_layout(
         plot_bgcolor=colors['background'],
         paper_bgcolor=colors['background'],
         font_color=colors['text'],
+        margin=dict(l=20, r=20, t=35, b=20)
         )
 
         df=openfiles.updata_df_sound(3)
