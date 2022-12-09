d6dd08593f9f227872014abab2b15af53b33498c
Simon Sunesen Gaasdal <sgaasd19@student.aau.dk>
2022-12-09 12:24:01 +0100

    sound timestamp fixed

--- timetest.py
+++ timetest.py
@@ -18,13 +18,13 @@ import openfiles
 #print((unix_time_end-unix_time_start)/1e3)
 
 lines = []
-with open("Data/weld/2022128_1_weld_004.csv", 'r') as file:
+with open("Data/sound/2022128_1_sound_001.csv", 'r') as file:
     data = csv.reader(file)
     for entry in data:
         lines.append(entry)
 
 
-unix_start = 1670517862 * 1000
+unix_start = 1670517447 * 1000
 weld_time_seconds = 28400
 unix_end = unix_start + weld_time_seconds
 
@@ -33,7 +33,7 @@ print(unix_end)
 print(unix_end-unix_start)
 
 df_meta=openfiles.updata_df(1)
-df=openfiles.df_from_path(openfiles.col_to_list(df_meta,'Path_weld'),3-1)
+df=openfiles.df_from_path(openfiles.col_to_list(df_meta,'Path_sound'),0-1)
 print(df)
 
 lst=np.linspace(unix_start,unix_end,len(df.index))
@@ -41,5 +41,5 @@ lst=np.linspace(unix_start,unix_end,len(df.index))
 
 df['time [s]']=lst
 
-df.to_csv("Data/weld/2022128_1_weld_004.csv",index=False)
+df.to_csv("Data/sound/2022128_1_sound_001.csv",index=False)
 
